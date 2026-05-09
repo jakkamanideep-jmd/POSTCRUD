8c16f4f912deb895f310a1abe573a5c37152fa78
manideep <jakkamanideep@gmail.com>
2026-05-09 02:51:54 -0700

    some changes

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -12,7 +12,7 @@ class User(db.Model,UserMixin):
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(10),nullable=True,unique=True)
     email=db.Column(db.String(120),nullable=True,unique=True)
-    image_file=db.Column(db.String(20),nullable=False,default="default.jpg")
+    image_file=db.Column(db.String(20),nullable=False,default="default.jpeg")
     password=db.Column(db.String(60),nullable=True)
     posts=db.relationship("Post",backref="author",lazy=True)
     
