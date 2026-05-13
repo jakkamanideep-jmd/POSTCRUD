02e8657de0c6e6dd316e56b4ea30399daec1e51a
manideep <jakkamanideep@gmail.com>
2026-05-13 03:48:21 -0700

    some changs

--- POSTCRUD/config.py
+++ POSTCRUD/config.py
@@ -3,7 +3,7 @@ import os
 class config():
     SECRET_KEY = '4631a00de3e108827d1ca43f00c468863ecc9f84'
     SQLALCHEMY_DATABASE_URI="sqlite:///site.db"
-    MAIL_SERVER="smtp.googlemail.com"
+    MAIL_SERVER="smtp.gmail.com"
     MAIL_PORT=587
     MAIL_USE_TLS=True
     MAIL_USERNAME="jakkamanideep@gmail.com"
