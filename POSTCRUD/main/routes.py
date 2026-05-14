8d82872af04e60e1c890518b122f55d94e738a2f
manideep <jakkamanideep@gmail.com>
2026-05-14 09:47:16 -0700

    some chamges

--- POSTCRUD/main/routes.py
+++ POSTCRUD/main/routes.py
@@ -8,5 +8,5 @@ main=Blueprint("main",__name__)
 @main.route("/home")
 def home():
     page=request.args.get("page",1,type=int)
-    posts=Post.query.order_by(Post.date_posted.desc()).paginate(per_page=1,page=page)
+    posts=Post.query.order_by(Post.date_posted.desc()).paginate(per_page=3,page=page)
     return render_template("home.html",title="home_page",posts=posts)
